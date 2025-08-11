a8691097c7a47a9a3a32f69325439c4c57001b7e
Aiman Rosli <aimanrosli977@yahoo.com>
2025-08-12 01:20:58 +0800

    Complete PKCE Authorisation Code Flow
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- oauth2-pkce-demo/src/main/resources/frontend/src/app/app.component.ts
+++ oauth2-pkce-demo/src/main/resources/frontend/src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
 import { authConfig } from './auth.config';
+import { AppService } from './app.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'frontend';
+  text = '';
+  helloSubscription: Subscription;
 
-  constructor(private oauthService: OAuthService) {
+  constructor(private oauthService: OAuthService, private appService: AppService) {
     this.configure();
+    this.helloSubscription = this.appService.hello().subscribe(response => {
+    this.text = response;
+    })
+  }
+
+  ngOnDestroy(): void {
+       this.helloSubscription.unsubscribe();
   }
 
   private configure(){
